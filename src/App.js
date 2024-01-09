9804c39474e77209e20cb43e4e1f033649dd6315
diegocallegari96 <warsindustry@gmail.com>
2024-01-09 13:23:12 +0700

    workk

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Skills from "./components/Skills";
-import Work from "./components/Work";
+import Work from "./components/Work"
 
 function App() {
   return (
@@ -11,7 +11,6 @@ function App() {
       <Home />
       <About />
       <Skills />
-      <Work />
     </div>
   );
 }
