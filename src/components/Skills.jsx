3d434a0067655f739987b454c2f9f7a98b4b2d24
diegocallegari96 <warsindustry@gmail.com>
2024-01-09 13:13:27 +0700

    skills logos

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,18 +8,38 @@ import GitHub from '../assets/github.png'
 
 const Skills = () => {
   return (
-    <div name='skills' className='bg-[#0a192f]'>
+    <div name='skills' className='bg-[#0a192f] text-gray-300'>
         {/* container */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full '>
             <div>
-                <p className='text-4xl font-bold inline border-b-4 border-[#9049e4] text-gray-300 py-4'>Experience</p>
-                <p>These are the technologies i worked with</p>
+                <p className='text-4xl font-bold inline border-b-4 border-[#9049e4]'>Experience</p>
+                <p className='py-4'>These are the technologies i worked with</p>
             </div>
-
-            <div>
-                <div>
+                {/* col aanpassen */}
+            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
-                    <p>HTML</p>
+                    <p className='my-4'>HTML</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
+                    <p className='my-4'>CSS</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={JavaScript} alt="Javascript icon" />
+                    <p className='my-4'>JAVASCRIPT</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={ReactIMG} alt="React icon" />
+                    <p className='my-4'>REACT</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Node} alt="Node JS icon" />
+                    <p className='my-4'>NODE JS</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={GitHub} alt="Github icon" />
+                    <p className='my-4'>GITHUB</p>
                 </div>
             </div>
         </div>
