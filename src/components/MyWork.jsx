73248a313a3ff4a151f33df119d3ea54d52f26d3
diegocallegari96 <warsindustry@gmail.com>
2024-09-17 11:30:55 +0700

    new work

--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import WeatherAppImg from '../assets/ThailandWeatherApp.png'
+import Seraphine from '../assets/serphinescents'
 
 const MyWork = () => {
   return (
@@ -13,22 +13,18 @@ const MyWork = () => {
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
                 {/* grid item */}
                 <div 
-                style={{backgroundImage: `url(${WeatherAppImg})`}} 
+                style={{backgroundImage: `url(${Seraphine})`}} 
                 className='shadow-lg shadow-[#040c16 group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* hover effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
-                            React JS Application
+                            Seraphine Scent
                         </span>
                         <div className='pt-8 text-center'>
-                            <a href="/">
+                            <a href="https://serphinescents.com/">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href="https://github.com/diegocallegari96/ThailandWeatherApp.github.io" target='blank'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                            </a>
-
                         </div>
                     </div>
                 </div>
